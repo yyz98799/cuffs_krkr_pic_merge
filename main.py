7cfe78856897eb19bb709f707fe987862f7fae74
yyz98799 <IzumiHoshi@163.com>
2023-03-26 17:50:07 +0800

    feat: support jpg, add alpha channel for png format

--- main.py
+++ main.py
@@ -12,16 +12,14 @@ def pic_merge(base, diff, pos):
     y2 = pos[1]+pos[3]
     pic1_path = os.path.join(path, pic_dict[base])
     pic2_path = os.path.join(path, pic_dict[diff])
-    pic1 = cv2.imread(pic1_path)
-    pic2 = cv2.imread(pic2_path)
+    pic1 = cv2.imread(pic1_path, cv2.IMREAD_UNCHANGED)
+    pic2 = cv2.imread(pic2_path, cv2.IMREAD_UNCHANGED)
 
     pic1[y1:y2, x1:x2] = pic2
-    # cv2.imshow('1', pic1)
-    # cv2.waitKey()
     save_path = './pic_out'
-    # save_url = os.path.join(save_path, base.lower() + '_' + diff.lower()+'.png')
-    save_url = os.path.join(save_path, diff.lower()+'.png')
+    save_url = os.path.join(save_path, pic_dict[diff].split('.')[0] + '.png')
     print(save_url)
+    # save as RGBA
     cv2.imwrite(save_url, pic1)
 
 
@@ -30,11 +28,13 @@ os.makedirs('pic_out', exist_ok=True)
 file_list = os.listdir(path)
 pic_dict = {}
 csv_list = []
+image_format = ['png', 'jpg', 'bmp']
 
 for i in file_list:
-    if i[-8:] == '.tlg.png':
-        pic_dict[i[:-8].upper()] = i
-    if i[-4:] == '.csv':
+    tmp_sp = i.split('.')
+    if tmp_sp[-1] in image_format:
+        pic_dict[tmp_sp[0].upper()] = i
+    if tmp_sp[-1] == 'csv':
         csv_list.append(i)
 
 for csv_file in csv_list:
@@ -48,10 +48,14 @@ for csv_file in csv_list:
             # copy base pic
             if diff == '':
                 tmp_str = pic_dict[base]
-                tmp_str = tmp_str[:-8] + tmp_str[-4:]
+                tmp_sp = tmp_str.split('.')
+                # tmp_str = tmp_sp[0] + '.' + tmp_sp[-1]
                 sour = os.path.join(path, pic_dict[base])
-                dest = os.path.join('./pic_out', tmp_str)
-                shutil.copyfile(sour, dest)
+                dest = os.path.join('./pic_out', tmp_sp[0]+'.png')
+                # cv2.imread(sour, cv2.IMREAD_UNCHANGED)
+                cv2.imwrite(dest, cv2.imread(sour, cv2.IMREAD_UNCHANGED))
+                # shutil.copyfile(sour, dest)
+                print("copy: " + dest)
                 continue
             pos = [int(row[3]), int(row[4]), int(row[5]), int(row[6])]
             pic_merge(base, diff, pos)
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
-用于合成cuffs系（Cuffs, Sphere, CUBE, mintCUBE, sonora; 有名字不代表可以使用，具体自行测试）以及HULOTTE社，使用krkr引擎的较新作品的CG和立绘合成。  
+用于合成cuffs系（Cuffs, Sphere, CUBE, mintCUBE, Sonora; 有名字不代表可以使用，具体自行测试）以及HULOTTE社，使用krkr引擎的较新作品的CG和立绘合成。  
 建议和[KrkrExtract](https://github.com/xmoeproject/KrkrExtract) 或[GARbro](https://github.com/morkt/GARbro) 配合使用。  
 本质是汉化工作的副产物，大概不怎么会更新。  
+## 更新
+* 230326 CUBE新作将large size的CG改为jpg格式，修改了代码以支持不同文件格式的图片合成  
+* 230326 将合成结果改为有透明通道的png格式
+* 230326 测试了更多游戏
 ## requirements  
 安装Python3环境，随后在命令行运行
 ```
@@ -12,13 +16,18 @@ pip install opencv_python
 ```
 python main.py
 ```
-会自动依照目录下的csv文件（.csv）和.tlg文件转换后的.png文件（.tlg.png，如不是此格式自己修改代码）合成，合成结果在pic_out文件夹内
+会自动依照目录下的csv文件（.csv）和.tlg文件转换后的.png文件（.tlg.png）或.jpg文件（夏ノ終熄之后的CUBE作品，large size的CG为jpg格式）合成，合成结果在pic_out文件夹内
 ## 已知可用游戏
-* [200327][CUBE] 神様のような君へ
-* [220325][CUBE] 神様のような君へ Extended Edition
-* [201127][Sonora] 響野さん家はエロゲ屋さん！
-* [211126][Sonora] ウチはもう、延期できない。
-* [210924][HULOTTE] 俺の恋天使がポンコツすぎてコワ～い。
+* [CUBE][200327] 神様のような君へ
+* [CUBE][220325] 神様のような君へ Extended Edition
+* [CUBE][220527] ネコと女子寮（ワカイ）せよ！
+* [CUBE][220826] 夏ノ終熄
+* [CUBE][221125] サメと生きる七日間
+* [Sonora][201127] 響野さん家はエロゲ屋さん！
+* [Sonora][211126] ウチはもう、延期できない。
+* [HULOTTE][200327] 俺の姿が、透明に！？ 不可視の薬と数奇な運命
+* [HULOTTE][210924] 俺の恋天使がポンコツすぎてコワ～い。
+
 ## 注意事项
-* 合成出来是没有透明通道的，懒得改了，需要透明通道的立绘自己转换一下  
+* ~~合成出来是没有透明通道的，懒得改了，需要透明通道的立绘自己转换一下。~~ 已经修改为有透明通道了  
 * 没有技术力，欢迎issue和pr，大佬们见笑了  
